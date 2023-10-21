06462a4ef943e2f840738a2f8848f938048f864d
Dmitry <dfarm8774@gmail.comclear>
2023-10-21 11:50:04 +0700

    final

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function showNews(blockId) {
+    let blockToShow = document.getElementById(blockId);
+    blockToShow.style.display = 'flex';
+}
+
+function hideNews(blockId) {
+    let block = document.getElementById(blockId);
+    block.style.display = 'none';
+}
